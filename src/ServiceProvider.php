86989fee4cddc15a713f4de53a6a074a59e0b96a
linsunnyday <linsunnyday@gmail.com>
2019-08-20 09:46:30 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -18,12 +18,11 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     /**
      * Laravel 扩展包的延迟注册方式，它不会在框架启动就注册，而是当你调用到它的时候才会注册.
-     * 
+     *
      * @return [type] [description]
      */
     public function provides()
     {
         return [Weather::class, 'weather'];
     }
-    
 }
