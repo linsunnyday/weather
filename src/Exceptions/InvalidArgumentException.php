6f9e6651e96d2d3581c15cd67ec193c502ff71d4
linsunnyday <linsunnyday@gmail.com>
2019-08-20 09:12:20 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidArgumentException.php
+++ src/Exceptions/InvalidArgumentException.php
@@ -3,9 +3,8 @@
 namespace Linsunnyday\Weather\Exceptions;
 
 /**
- * 当调用方传递的 $format 不是 xml 也不是 json 时需要抛出参数异常
+ * 当调用方传递的 $format 不是 xml 也不是 json 时需要抛出参数异常.
  */
 class InvalidArgumentException extends Exception
 {
-
 }
